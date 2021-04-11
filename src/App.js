03a118e5d1fbd53527890d30e352d0fed58f0ebd
Agilan Vageesan <agilanvageesan@gmail.com>
2021-04-11 12:24:06 +0530

    feat(master): initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> welcome to covid tracker</h1>
+     {/* Header
+     Title + select input dropdown fied
+     infoboxes
+     table 
+     graph
+     map */}
+    </div>
+  );
+}
+
+export default App;
