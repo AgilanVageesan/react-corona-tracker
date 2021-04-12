f4a61727befc0b1df7f250d94592d3f41bbbd233
Agilan Vageesan <agilanvageesan@gmail.com>
2021-04-12 13:33:44 +0530

    build

---
+++ build/precache-manifest.88b5237fd5996617833a479bc03a7c17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6885e8daa50ca859b3d67b9a3e73aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d7d375ef0d7e8ca00c8",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "1719e9df79a48ba1e54d",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "5d7d375ef0d7e8ca00c8",
+    "url": "/static/js/2.c566785b.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.c566785b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1719e9df79a48ba1e54d",
+    "url": "/static/js/main.35f9e8dd.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
